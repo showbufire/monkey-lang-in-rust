59b72d185a2569875db5fec2d0008af5f39f662b
Xiao Jiang <showbufire@gmail.com>
2020-06-21 11:18:54 -0700

    [lexer] pass tests with ident and ints

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,6 @@
+const LET: &str = "let";
+const FUNCTION: &str = "fn";
+
 #[derive(Debug, PartialEq)]
 pub enum Token {
     ASSIGN,
@@ -12,6 +15,7 @@ pub enum Token {
     LET,
     FUNCTION,
     IDENT(String),
+    INT(i64),
 }
 
 pub struct Lexer {
@@ -44,7 +48,48 @@ impl Lexer {
         self.ch
     }
 
+    fn peek_char(&self) -> u8 {
+        if self.read_pos >= self.input.len() {
+            0
+        } else {
+            self.input.as_bytes()[self.read_pos]
+        }
+    }
+
+    fn next_identifier_or_keyword(&mut self) -> Token {
+        let mut bytes = vec![];
+        loop {
+            bytes.push(self.ch);
+            if self.peek_char().is_ascii_alphabetic() {
+                self.read_char();
+            } else {
+                break;
+            }
+        }
+        let ident_or_keyword = String::from_utf8(bytes).unwrap();
+        match ident_or_keyword.as_str() {
+            LET => Token::LET,
+            FUNCTION => Token::FUNCTION,
+            _ => Token::IDENT(ident_or_keyword),
+        }
+    }
+
+    fn next_int(&mut self) -> Token {
+        let mut x: i64 = 0;
+        loop {
+            x = x * 10 + (self.ch - b'0') as i64;
+            if self.peek_char().is_ascii_digit() {
+                self.read_char();
+            } else {
+                break;
+            }
+        }
+        Token::INT(x)
+    }
+
     pub fn next_token(&mut self) -> Token {
+        self.skip_whitespaces();
+
         let token = match self.ch {
             b'=' => Token::ASSIGN,
             b'+' => Token::PLUS,
@@ -54,11 +99,23 @@ impl Lexer {
             b'}' => Token::RBRACE,
             b',' => Token::COMMA,
             b';' => Token::SEMICOLON,
+            b'a'..= b'z' | b'A'..= b'Z'=> self.next_identifier_or_keyword(),
+            b'0'..= b'9' => self.next_int(),
             0 => Token::EOF,
             _ => panic!("unknown char {}", String::from_utf8(vec![self.ch]).unwrap()),
         };
         self.read_char();
         token
+    }
+
+    fn skip_whitespaces(&mut self) {
+        while true {
+            match self.ch {
+                b' ' | b'\t' | b'\n' => (),
+                _ => break,
+            }
+            self.read_char();
+        }
     }
 }
 
@@ -90,7 +147,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_next_token_with_ident() {
         let input = String::from("let five = 5;
         let ten = 10;
@@ -101,10 +157,48 @@ mod tests {
         let mut lexer = Lexer::new(input);
 
         let expected = vec![
+            Token::LET,
+            Token::IDENT(String::from("five")),
+            Token::ASSIGN,
+            Token::INT(5),
+            Token::SEMICOLON,
+            Token::LET,
+            Token::IDENT(String::from("ten")),
+            Token::ASSIGN,
+            Token::INT(10),
+            Token::SEMICOLON,
+            Token::LET,
+            Token::IDENT(String::from("add")),
+            Token::ASSIGN,
+            Token::FUNCTION,
+            Token::LPAREN,
+            Token::IDENT(String::from("x")),
+            Token::COMMA,
+            Token::IDENT(String::from("y")),
+            Token::RPAREN,
+            Token::LBRACE,
+            Token::IDENT(String::from("x")),
+            Token::PLUS,
+            Token::IDENT(String::from("y")),
+            Token::SEMICOLON,
+            Token::RBRACE,
+            Token::SEMICOLON,
+            Token::LET,
+            Token::IDENT(String::from("result")),
+            Token::ASSIGN,
+            Token::IDENT(String::from("add")),
+            Token::LPAREN,
+            Token::IDENT(String::from("five")),
+            Token::COMMA,
+            Token::IDENT(String::from("ten")),
+            Token::RPAREN,
+            Token::SEMICOLON,
+            Token::EOF,
         ];
 
         for expected_token in expected {
             let token = lexer.next_token();
+            println!("{:?} {}", token, String::from_utf8(vec![lexer.ch]).unwrap());
             assert_eq!(token, expected_token);
         }
     }
